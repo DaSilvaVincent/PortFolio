7e4f6d1ba6515f4cb052faada647abb5433a215f
DaSilvaVincent <106517502+DaSilvaVincent@users.noreply.github.com>
2023-01-03 10:45:27 +0100

    Ajout du code de mon portfolio

---
+++ app.js
@@ -0,0 +1,38 @@
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
+
+function myFunction1() {
+    var popup = document.getElementById("myPopup1");
+    popup.classList.toggle("show");
+}
+function myFunction2() {
+    var popup = document.getElementById("myPopup2");
+    popup.classList.toggle("show");
+}
+function myFunction3() {
+    var popup = document.getElementById("myPopup3");
+    popup.classList.toggle("show");
+}
+function myFunction4() {
+    var popup = document.getElementById("myPopup4");
+    popup.classList.toggle("show");
+}
+function myFunction5() {
+    var popup = document.getElementById("myPopup5");
+    popup.classList.toggle("show");
+}
+function myFunction6() {
+    var popup = document.getElementById("myPopup6");
+    popup.classList.toggle("show");
+}
